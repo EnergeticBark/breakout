10c0ee5e52e3511b162dca75debd08102536ee56
EnergeticBark <seth.humphries@live.com>
2024-05-20 20:03:18 +0100

    Fixed collision resolution when velocity on an axis is 0. Added test case for an object moving up.

--- src/main/java/Collision.java
+++ src/main/java/Collision.java
@@ -6,43 +6,44 @@ public class Collision {
 
     // Figure out which side(s) the collision occurred and how far the moving object passed through the other.
     Collision(KineticGameObj moving, GameObj stationary) {
-        int movingXSide;
-        int stationaryXSide;
+        float xSideT = Float.NEGATIVE_INFINITY;
+        float ySideT = Float.NEGATIVE_INFINITY;
+
         if (moving.velocityX > 0) { // Moving right
             // Check moving's previous right side against stationary's left side.
-            movingXSide = moving.right() - moving.velocityX;
-            stationaryXSide = stationary.left();
+            final int previousRight = moving.right() - moving.velocityX;
+            xSideT = timeOfAxisCollision(stationary.left(), previousRight, moving.velocityX);
             xPenetration = moving.right() - stationary.left() - 1;
-        } else {
+        } else if (moving.velocityX < 0) { // Moving left
             // Check moving's previous left side against stationary's right side.
-            movingXSide = moving.left() - moving.velocityX;
-            stationaryXSide = stationary.right();
+            final int previousLeft = moving.left() - moving.velocityX;
+            xSideT = timeOfAxisCollision(stationary.right(), previousLeft, moving.velocityX);
             xPenetration = moving.left() - stationary.right() + 1;
         }
 
-        int movingYSide;
-        int stationaryYSide;
         if (moving.velocityY > 0) { // Moving down
             // Check moving's previous bottom side against stationary's top side.
-            movingYSide = moving.bottom() - moving.velocityY;
-            stationaryYSide = stationary.top();
+            final int previousBottom = moving.bottom() - moving.velocityY;
+            ySideT = timeOfAxisCollision(stationary.top(), previousBottom, moving.velocityY);
             yPenetration = moving.bottom() - stationary.top() - 1;
-        } else {
+        } else if (moving.velocityY < 0) { // Moving up
             // Check moving's previous top side against stationary's bottom side.
-            movingYSide = moving.top() - moving.velocityY;
-            stationaryYSide = stationary.bottom();
+            final int previousTop = moving.top() - moving.velocityY;
+            ySideT = timeOfAxisCollision(stationary.bottom(), previousTop, moving.velocityY);
             yPenetration = moving.top() - stationary.bottom() + 1;
         }
 
-        // Find the time of intersection on each axis.
-        // Solve for t:
-        // movingSide + velocity*t = stationarySide
-        final float xSideT = (float) (stationaryXSide - movingXSide) / moving.velocityX;
-        final float ySideT = (float) (stationaryYSide - movingYSide) / moving.velocityY;
-
-        // The collision happened on whichever axis was intersected last.
+        /* The collision happened on whichever axis was intersected last, or both if they happened at the same time
+        (a corner was hit). If velocity on an axis is 0, time of collision on that axis will be -infinity. */
         hitX = xSideT >= ySideT;
         hitY = xSideT <= ySideT;
+    }
+
+    // Find the time of collision on one axis.
+    // Solve for t:
+    // movingSide + velocity*t = stationarySide
+    private static float timeOfAxisCollision(int stationarySide, int movingSide, int velocity) {
+        return (float) (stationarySide - movingSide) / velocity;
     }
 }
 
--- src/test/java/CollisionTest.java
+++ src/test/java/CollisionTest.java
@@ -10,11 +10,25 @@ class CollisionTest {
         GameObj stationary = new GameObj(0, 0, 10, 10, null);
         KineticGameObj moving = new KineticGameObj(5, 0, 10, 10, null);
         moving.velocityX = -5;
-        moving.velocityY = 1;
+        moving.velocityY = 0;
         Collision collision = new Collision(moving, stationary);
         assertAll(
                 () -> assertTrue(collision.hitX),
                 () -> assertFalse(collision.hitY)
+        );
+    }
+
+    @Test
+    @DisplayName("KineticGameObj moving up and hitting bottom side of GameObj")
+    void movingUpHit() {
+        GameObj stationary = new GameObj(0, 0, 10, 10, null);
+        KineticGameObj moving = new KineticGameObj(0, 5, 10, 10, null);
+        moving.velocityX = 0;
+        moving.velocityY = -5;
+        Collision collision = new Collision(moving, stationary);
+        assertAll(
+                () -> assertFalse(collision.hitX),
+                () -> assertTrue(collision.hitY)
         );
     }
 }
