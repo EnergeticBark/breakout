f1f7332d2c6266eeba4fe87c4d8cbe0f90bc5799
EnergeticBark <seth.humphries@live.com>
2024-05-21 16:44:42 +0100

    Fixed tests not using Vector2 for positions.

--- src/test/java/CollisionTest.java
+++ src/test/java/CollisionTest.java
@@ -7,10 +7,14 @@ class CollisionTest {
     @Test
     @DisplayName("KineticGameObj moving left and hitting right side of GameObj")
     void movingLeftHit() {
-        GameObj stationary = new GameObj(0, 0, 10, 10, null);
-        KineticGameObj moving = new KineticGameObj(5, 0, 10, 10, null);
+        final Vector2 stationaryPosition = new Vector2(0, 0);
+        GameObj stationary = new GameObj(stationaryPosition, 10, 10, null);
+
+        final Vector2 movingPosition = new Vector2(5, 0);
+        KineticGameObj moving = new KineticGameObj(movingPosition, 10, 10, null);
         moving.velocityX = -5;
         moving.velocityY = 0;
+
         Collision collision = new Collision(moving, stationary);
         assertAll(
                 () -> assertTrue(collision.hitX),
@@ -21,10 +25,14 @@ class CollisionTest {
     @Test
     @DisplayName("KineticGameObj moving up and hitting bottom side of GameObj")
     void movingUpHit() {
-        GameObj stationary = new GameObj(0, 0, 10, 10, null);
-        KineticGameObj moving = new KineticGameObj(0, 5, 10, 10, null);
+        final Vector2 stationaryPosition = new Vector2(0, 0);
+        GameObj stationary = new GameObj(stationaryPosition, 10, 10, null);
+
+        final Vector2 movingPosition = new Vector2(0, 5);
+        KineticGameObj moving = new KineticGameObj(movingPosition, 10, 10, null);
         moving.velocityX = 0;
         moving.velocityY = -5;
+
         Collision collision = new Collision(moving, stationary);
         assertAll(
                 () -> assertFalse(collision.hitX),
